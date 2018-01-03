19bfb2b5b157b2441f4b120b9a8c52b3c68e8351
CoolSquid <mail@coolsquid.me>
2018-01-03 21:46:04 +0100

    Improved position-related utility methods.

--- src/main/java/coolsquid/react/util/Util.java
+++ src/main/java/coolsquid/react/util/Util.java
@@ -11,10 +11,35 @@ import net.minecraft.init.Items;
 import net.minecraft.inventory.EntityEquipmentSlot;
 import net.minecraft.item.Item;
 import net.minecraft.util.DamageSource;
+import net.minecraft.util.math.Vec3d;
 
 public class Util {
 
 	public static final Map<String, DamageSource> STATIC_DAMAGE_SOURCES = new HashMap<>();
+
+	public static Vec3d getCoordFromTarget(Object target, Map<String, Object> parameters) {
+		double x;
+		double y;
+		double z;
+		if (target instanceof EntityLivingBase) {
+			x = ((EntityLivingBase) target).posX;
+			y = ((EntityLivingBase) target).posY;
+			z = ((EntityLivingBase) target).posZ;
+		} else if (target instanceof BlockWrapper) {
+			x = ((BlockWrapper) target).pos.getX();
+			y = ((BlockWrapper) target).pos.getY();
+			z = ((BlockWrapper) target).pos.getZ();
+		} else if (target == null) {
+			x = ((Number) parameters.get("x")).doubleValue();
+			y = ((Number) parameters.get("y")).doubleValue();
+			z = ((Number) parameters.get("z")).doubleValue();
+		} else {
+			throw new RuntimeException(
+					"Target type " + target.getClass().getName() + " cannot be used with this action");
+		}
+		return new Vec3d(getRelativeNumber(parameters.get("x"), x), getRelativeNumber(parameters.get("y"), y),
+				getRelativeNumber(parameters.get("z"), z));
+	}
 
 	public static double getRelativeNumber(Object number, double oldNumber) {
 		if (number instanceof String) {
@@ -28,6 +53,8 @@ public class Util {
 			}
 		} else if (number instanceof Number) {
 			return ((Number) number).doubleValue();
+		} else if (number == null) {
+			return oldNumber;
 		}
 		Log.REACT.error("Failed to parse parameter: %s", number);
 		throw new RuntimeException("Failed to parse parameter");
